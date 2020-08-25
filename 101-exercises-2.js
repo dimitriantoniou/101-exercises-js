667e447942ff6259a5978d418b1412c64858751e
dimitriantoniou <dma2012@gmail.com>
2020-08-24 20:43:49 -0500

    #58
    Signed-off-by: dimitriantoniou <dma2012@gmail.com>

--- 101-exercises-2.js
+++ 101-exercises-2.js
@@ -856,7 +856,9 @@ addToDone("Exercise 57 is correct.")
 
 // Exercise 58
 // Write a function definition named firstAndLast that takes in sequence and returns the first and last value of that sequence as an array
-
+function firstAndLast(value){
+    return [value[0],value[value.length-1]];
+}
 assert(firstAndLast([1, 2, 3, 4]), [1, 4], "Exercise 58");
 assert(firstAndLast(["JS", "is", "awesome"]), ["JS", "awesome"], "Exercise 58");
 assert(firstAndLast(["strawberry", "kiwi", "mango", "guava"]), ["strawberry", "guava"], "Exercise 58");
