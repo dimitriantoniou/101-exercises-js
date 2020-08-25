ea444770cf515a59dff59f56d724ec37708769c2
dimitriantoniou <dma2012@gmail.com>
2020-08-24 20:41:36 -0500

    #55
    Signed-off-by: dimitriantoniou <dma2012@gmail.com>

--- 101-exercises-2.js
+++ 101-exercises-2.js
@@ -821,7 +821,9 @@ addToDone("Exercise 54 is correct.")
 
 // Exercise 55
 // Write a function definition named secondToLast that takes in sequence and returns the second to last value of that sequence.
-
+function secondToLast(value){
+    return value[value.length-2];
+}
 assert(secondToLast("ubuntu"), "t", "Exercise 55");
 assert(secondToLast([1, 2, 3, 4]), 3, "Exercise 55");
 assert(secondToLast(["JS", "is", "awesome"]), "is", "Exercise 55");
