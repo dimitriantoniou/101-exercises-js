148c636d100d32399155a5cd98cee45dec0dfd35
dimitriantoniou <dma2012@gmail.com>
2020-08-25 18:26:09 -0500

    commit through the command line

--- 101-exercises-2.js
+++ 101-exercises-2.js
@@ -957,7 +957,7 @@ addToDone("Exercise 64 is correct.")
 
 // Exercise 65
 // Write a function definition named getHighestNumber that takes in sequence of numbers and returns the largest number.
-
+function fajdl;fhdasfh
 
 assert(getHighestNumber([1, 2, 3]), 3, "Exercise 65");
 assert(getHighestNumber([1, 5, 2, 3, 4]), 5, "Exercise 65");
