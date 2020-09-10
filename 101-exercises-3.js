721f52b929236508a363910c981d96afaa4f4af7
Dimitri <dma2012@gmail.com>
2020-09-10 07:52:29 -0500

    #82

--- 101-exercises-3.js
+++ 101-exercises-3.js
@@ -1258,7 +1258,15 @@ addToDone("Exercise 81 is correct.")
 
 // Exercise 82
 // Write a function definition named longestString that takes in sequence of strings and returns the longest string in the array.
-
+function longestString(arr){
+    let longest = arr[0];
+    for(let i =0;i<arr.length;i++){
+        if(arr[i].length>longest.length){
+            longest=arr[i];
+        }
+    }
+    return longest;
+}
 assert(longestString(["kiwi", "mango", "strawberry"]), "strawberry", "Exercise 82");
 assert(longestString(["hello", "everybody"]), "everybody", "Exercise 82");
 assert(longestString(["mary", "had", "a", "little", "lamb"]), "little", "Exercise 82");
