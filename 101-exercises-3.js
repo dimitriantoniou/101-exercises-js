53926533bff5afd296f476a601c8e3a34d1a3990
Dimitri <dma2012@gmail.com>
2020-09-11 06:23:59 -0500

    #90

--- 101-exercises-3.js
+++ 101-exercises-3.js
@@ -1383,7 +1383,9 @@ addToDone("Exercise 89 is complete.")
 
 // Exercise 90
 // Write a function named getBookAuthor that takes in a object (the above declared book variable) and returns the author's name
-
+function getBookAuthor(object){
+    return object.author;
+}
 
 assert(getBookAuthor(book), "Frances Buontempo", "Exercise 90");
 addToDone("Exercise 90 is complete.")
