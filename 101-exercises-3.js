e8be29a4f85c02bf3e066c810399b31e9f5090c9
Dimitri <dma2012@gmail.com>
2020-09-12 09:20:29 -0500

    #99

--- 101-exercises-3.js
+++ 101-exercises-3.js
@@ -1575,7 +1575,14 @@ addToDone("Exercise 98 is complete.")
 // Exercise 99
 // Write a function named getAverageItemPrice that takes in the shopping cart as an input and returns the average of all the item prices.
 // Hint - This should determine the total price divided by the number of types of items. This does not account for each item type's quantity.\
-
+function getAverageItemPrice(shoppingCart){
+    let numOfTypes = shoppingCart.items.length;
+    let sumOfPrices = 0;
+    for (let i =0;i<shoppingCart.items.length;i++){
+        sumOfPrices += shoppingCart.items[i].price;
+    }
+    return sumOfPrices/numOfTypes;
+}
 assert(getAverageItemPrice(shoppingCart), 2.1420000000000003, "Exercise 99");
 addToDone("Exercise 99 is complete.")
 
